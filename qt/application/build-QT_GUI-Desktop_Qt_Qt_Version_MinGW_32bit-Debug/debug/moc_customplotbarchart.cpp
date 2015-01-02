7b224c9d862cf16d4fba8736b4162dbe52f94ce8
SaurerDrop <janina-sauer@web.de>
2015-01-02 16:55:18 +0100

    Präsentation eingefügt

---
+++ qt/application/build-QT_GUI-Desktop_Qt_Qt_Version_MinGW_32bit-Debug/debug/moc_customplotbarchart.cpp
@@ -0,0 +1,159 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'customplotbarchart.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.4.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../QT_GUI/Diagram/customplotbarchart.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'customplotbarchart.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.4.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_CustomPlotBarChart_t {
+    QByteArrayData data[6];
+    char stringdata[70];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_CustomPlotBarChart_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_CustomPlotBarChart_t qt_meta_stringdata_CustomPlotBarChart = {
+    {
+QT_MOC_LITERAL(0, 0, 18), // "CustomPlotBarChart"
+QT_MOC_LITERAL(1, 19, 14), // "initCustomPlot"
+QT_MOC_LITERAL(2, 34, 0), // ""
+QT_MOC_LITERAL(3, 35, 16), // "updateDataAndGUI"
+QT_MOC_LITERAL(4, 52, 4), // "data"
+QT_MOC_LITERAL(5, 57, 12) // "SensorModel*"
+
+    },
+    "CustomPlotBarChart\0initCustomPlot\0\0"
+    "updateDataAndGUI\0data\0SensorModel*"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_CustomPlotBarChart[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       1,   26, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // methods: name, argc, parameters, tag, flags
+       1,    0,   24,    2, 0x02 /* Public */,
+       3,    0,   25,    2, 0x02 /* Public */,
+
+ // methods: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+
+ // properties: name, type, flags
+       4, 0x80000000 | 5, 0x0009510b,
+
+       0        // eod
+};
+
+void CustomPlotBarChart::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        CustomPlotBarChart *_t = static_cast<CustomPlotBarChart *>(_o);
+        switch (_id) {
+        case 0: _t->initCustomPlot(); break;
+        case 1: _t->updateDataAndGUI(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterPropertyMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 0:
+            *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< SensorModel* >(); break;
+        }
+    }
+
+}
+
+const QMetaObject CustomPlotBarChart::staticMetaObject = {
+    { &QQuickPaintedItem::staticMetaObject, qt_meta_stringdata_CustomPlotBarChart.data,
+      qt_meta_data_CustomPlotBarChart,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *CustomPlotBarChart::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *CustomPlotBarChart::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_CustomPlotBarChart.stringdata))
+        return static_cast<void*>(const_cast< CustomPlotBarChart*>(this));
+    return QQuickPaintedItem::qt_metacast(_clname);
+}
+
+int CustomPlotBarChart::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QQuickPaintedItem::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 2;
+    }
+#ifndef QT_NO_PROPERTIES
+      else if (_c == QMetaObject::ReadProperty) {
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: *reinterpret_cast< SensorModel**>(_v) = getData(); break;
+        default: break;
+        }
+        _id -= 1;
+    } else if (_c == QMetaObject::WriteProperty) {
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: setData(*reinterpret_cast< SensorModel**>(_v)); break;
+        default: break;
+        }
+        _id -= 1;
+    } else if (_c == QMetaObject::ResetProperty) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyDesignable) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyScriptable) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyStored) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyEditable) {
+        _id -= 1;
+    } else if (_c == QMetaObject::QueryPropertyUser) {
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterPropertyMetaType) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    }
+#endif // QT_NO_PROPERTIES
+    return _id;
+}
+QT_END_MOC_NAMESPACE
